54eed569c5ca68d1dfe41dfba386f1c80ac3d50d
Dario Sekelj <102439860+DarioSekelj@users.noreply.github.com>
2022-03-31 00:01:40 +0200

    Update index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,5 +1,11 @@
 function main() {
+  //this is a single line JavaScript comment
   console.log(`Hello World!`);
+  /* document.write("this is the multi=line comment!");
+  document.write("this is the multi=line comment!");
+  */
+ var forCat;
+ 
 }
-main();
+console.log(main());
 module.exports = main;
